5699210ccf6ccbf72c25aef2baea06313c504416
ssbzwane-afk <ssbzwane@gmail.com>
2026-06-22 13:51:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('ITD Website Loaded');
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+a.addEventListener('click',e=>{
+e.preventDefault();
+document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+});
+});
